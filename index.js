c01c2d753104b9cb8f0138c12a3b7ae504eddf29
SanjeevRai07 <sr1415014@gmail.com>
2026-06-05 11:31:53 +0530

    Add Number divisible by 5

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Sanjeev Rai")
+let name = prompt("Enter a number:");
+if(name % 5 ===0){
+    console.log("The number is divisible by 5");
+}else{    console.log("The number is not divisible by 5");
+}
